c9615798d09f46380660090960a1241755fd0af2
mitchell-917 <mitchell.perry001@gmail.com>
2021-04-04 11:26:05 +0100

    Saving Progress

--- Web_Scraper.py
+++ Web_Scraper.py
@@ -1,3 +1,15 @@
-test = 2 
+import bs4
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
 
+# Open and Close connection
+my_url = 'https://www.dailyfx.com/sentiment-report'
+uClient = uReq(my_url) # 
+page_html = uClient.read()
+uClient.close()
+
+# HTML Parsing
+page_soup = soup(page_html, "html.parser" )
+
+page_soup.h1
 
